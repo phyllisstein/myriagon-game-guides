59228451d1244594aa4cfd2fb4f40087340d6757
Daniel P. Shannon <daniel.shannon@voxmedia.com>
2023-01-01 12:51:04 -0500

    🛂 Load Tower tokens from the cache.

--- src/lib/content/tower-client.ts
+++ src/lib/content/tower-client.ts
@@ -1,4 +1,9 @@
 import { env } from '$env/dynamic/private'
+import fs from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 import {
   ApolloClient,
@@ -11,21 +16,38 @@ import {
 
 let client: ApolloClient<NormalizedCacheObject>
 
+async function getTowerToken (): Promise<string> {
+  try {
+    const rawCache = await fs.readFile(path.resolve(__dirname, '../../../.chorus/cache.json'), 'utf8')
+    const cache = JSON.parse(rawCache)
+
+    const credentials = cache?.__data['https://auth.voxmedia.com:client_credentials:v1']
+    const expiry = credentials?.expires
+      ? new Date(credentials.expires * 1000).getTime() - Date.now()
+      : 0
+    if (expiry < 0) {
+      throw new Error('Tower token has expired')
+    }
+
+    return credentials?.data
+  } catch (error) {
+    console.error(`Error reading Tower token: ${ error }`)
+    throw error
+  }
+}
+
 async function getClient () {
   if (!client) {
     const {
-      TOWER_TOKEN,
       TOWER_URI = 'https://tower.voxmedia.com/graphql',
     } = process.env
 
-    if (!TOWER_TOKEN) {
-      throw new Error('TOWER_TOKEN is required')
-    }
+    const token = await getTowerToken()
 
     client = new ApolloClient({
       cache: new InMemoryCache(),
       headers: {
-        Authorization: `Bearer ${ TOWER_TOKEN }`,
+        Authorization: `Bearer ${ token }`,
       },
       uri: TOWER_URI,
     })
